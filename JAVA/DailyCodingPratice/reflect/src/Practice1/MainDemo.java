1ee28952c6bf2b1addc6b872e195df74238d4127
冯帅 <44635374+FENGS-GITHUB@users.noreply.github.com>
2023-01-19 23:15:06 +0800

    Update MainDemo.java

--- JAVA/DailyCodingPratice/reflect/src/Practice1/MainDemo.java
+++ JAVA/DailyCodingPratice/reflect/src/Practice1/MainDemo.java
@@ -14,6 +14,14 @@ public class MainDemo {
         /*获取构造函数*/
         Constructor A = testclass1.getDeclaredConstructor(int.class);
         System.out.println(A);
+        /*构造函数对象 创建一个对象*/
+        Object o = A.newInstance(1);
+        Testclass1 t1=(Testclass1)o;
+        System.out.println(t1.testfield1);
+
+        /*通过new 建立一个实例对象并使用*/
+        Testclass1 t2=new Testclass1(1);
+        System.out.println(t2.testfield1);
     }
 
 }
